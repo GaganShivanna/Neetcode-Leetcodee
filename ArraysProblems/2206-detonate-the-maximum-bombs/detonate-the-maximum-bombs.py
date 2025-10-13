66fff92ac2ea793f04b367eb7880d6717d2c6936
GaganShivanna <98219082+GaganShivanna@users.noreply.github.com>
2025-10-12 21:47:33 -0700

    Time: 307 ms (78.98%) | Memory: 18.3 MB (45.41%) - LeetSync

---
+++ ArraysProblems/2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maximumDetonation(self, bombs: List[List[int]]) -> int:
+        n =len(bombs)
+
+        #Step 1: Building the directed graph by making use of adjacency list 
+        graph = [[] for _ in range(n)]
+        for i in range(n):
+            xi, yi, ri = bombs[i]
+            radius = ri * ri 
+            for j in range(n):
+                if i == j: 
+                    continue 
+                xj, yj, rj = bombs[j]
+                dx,dy = xi - xj, yi - yj 
+                if dx * dx + dy * dy <= radius : 
+                    graph[i].append(j)
+
+        #Step 2: Run the DFS traversal to find all the bombs that are reachable to detonate 
+
+        def dfs(node, visited):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei not in visited:
+                    dfs(nei, visited)
+
+        #step 3 : Trying to detonate the Maximum Bomb 
+        maxDetonate = 0 
+        for i in range(n):
+            visited = set()
+            dfs(i, visited)
+            maxDetonate = max(maxDetonate, len(visited)) 
+
+        return maxDetonate 
