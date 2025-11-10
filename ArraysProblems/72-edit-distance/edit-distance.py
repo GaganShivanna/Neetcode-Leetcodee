86606a3df3c8f6f02ce968d46a691ebd059e3810
GaganShivanna <98219082+GaganShivanna@users.noreply.github.com>
2025-11-10 15:48:58 -0800

    Time: 67 ms (49.58%) | Memory: 21.4 MB (15.27%) - LeetSync

--- ArraysProblems/72-edit-distance/edit-distance.py
+++ ArraysProblems/72-edit-distance/edit-distance.py
@@ -1,24 +1,19 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        m,n = len(word1), len(word2)
-        dp = [[0 for _ in range(n+1)]
-                for _ in range(m + 1)]
-        
-        #Check for the base criteria 
-        for i in range(m+1):
-            dp[i][0]= i 
-        for j in range(n+1):
-            dp[0][j] = j
+        m, n = len(word1), len(word2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
 
-        # Fill the DP table 
-        for i in range(1, m + 1):
-            for j in range(1, n + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j]= dp[i - 1][j - 1]
+        for i in range(m+1): 
+            dp[i][n] = m-i          # delete i chars
+        for j in range(n+1): 
+            dp[m][j] = n- j          # insert j chars
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1): 
+                if word1[i] == word2[j]:
+                    dp[i][j]=dp[i + 1][j + 1]
                 else:
-                    dp[i][j]= 1 + min(
-                                    dp[i][j-1],
-                                    dp[i-1][j],
-                                    dp[i-1][j-1]
-                    )
-        return dp[m][n]
+                    dp[i][j] = 1 + min( dp[i][j + 1],
+                                        dp[i + 1][j], 
+                                        dp[i + 1][j + 1])
+        return dp[0][0]
