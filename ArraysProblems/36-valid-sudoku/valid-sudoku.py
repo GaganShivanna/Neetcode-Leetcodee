21ef0a8b5031b963cb969d53d1c2febf5bc06ed1
Gagan Shivanna <98219082+GaganShivanna@users.noreply.github.com>
2026-04-13 15:23:14 -0700

    Time: 3 ms (76.16%) | Memory: 19.5 MB (18.46%) - LeetSync

--- ArraysProblems/36-valid-sudoku/valid-sudoku.py
+++ ArraysProblems/36-valid-sudoku/valid-sudoku.py
@@ -2,17 +2,17 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = collections.defaultdict(set)
         cols = collections.defaultdict(set)
-        innerSquares = collections.defaultdict(set)
+        innersquares = collections.defaultdict(set)
 
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
-                    continue
-                if (board[r][c] in rows[r] or 
+                if board[r][c] == "." : 
+                    continue 
+                if (board[r][c] in rows[r] or
                     board[r][c] in cols[c] or 
-                    board[r][c] in innerSquares[(r // 3, c // 3)]):
+                    board[r][c] in innersquares[(r//3, c//3)]):
                     return False
                 cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
-                innerSquares[(r // 3, c // 3)].add(board[r][c])
-        return True 
+                innersquares[(r//3, c//3)].add(board[r][c])
+        return True
