86a9c2807d38aa736990593793533ad5d918fcc1
Gagan Shivanna <98219082+GaganShivanna@users.noreply.github.com>
2026-05-04 16:21:10 -0700

    Time: 0 ms (100.00%) | Memory: 19.2 MB (73.05%) - LeetSync

--- ArraysProblems/14-longest-common-prefix/longest-common-prefix.py
+++ ArraysProblems/14-longest-common-prefix/longest-common-prefix.py
@@ -3,10 +3,9 @@ class Solution:
         if not strs:
             return ""
         strs.sort()
-        first = strs[0]
+        first=strs[0]
         last = strs[-1]
-
         i = 0
-        while i < len(first) and i < len(last) and first[i] == last[i]:
-            i += 1
+        while i < len(first) and i<len(last) and first[i] == last[i]:
+            i+= 1
         return last[:i]
